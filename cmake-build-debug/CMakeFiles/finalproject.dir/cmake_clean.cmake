8d40ca4f31dcdbc6e09aa6ee129cd2e5ab8e840c
rabah ghiles <gh.rabah29@gmail.com>
2019-12-16 16:01:50 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/finalproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finalproject.dir/main.cpp.obj"
+  "finalproject.exe"
+  "finalproject.exe.manifest"
+  "finalproject.lib"
+  "finalproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finalproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/geometric_algorithm_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geometric_algorithm_project.dir/main.cpp.obj"
+  "geometric_algorithm_project.exe"
+  "geometric_algorithm_project.exe.manifest"
+  "geometric_algorithm_project.lib"
+  "geometric_algorithm_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geometric_algorithm_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
